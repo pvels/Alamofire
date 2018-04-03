ed23dbb570485210337d0f67c57d0a6aa95fab18
Pavel Gerashchenko <p.gerashchenko@gmail.com>
2018-04-03 11:48:58 +0300

    Update MultipartFormData.swift

--- Source/MultipartFormData.swift
+++ Source/MultipartFormData.swift
@@ -276,18 +276,11 @@ open class MultipartFormData {
             setBodyPartError(withReason: .bodyPartFileIsDirectory(at: fileURL))
             return
         }
-
-        #if os(Linux) || os(Android) || os(Windows)
-            guard !isDirectory else {
-                setBodyPartError(withReason: .bodyPartFileIsDirectory(at: fileURL))
-                return
-            }
-        #else
-            guard !isDirectory.boolValue else {
-                setBodyPartError(withReason: .bodyPartFileIsDirectory(at: fileURL))
-                return
-            }
-        #endif
+        
+        guard !isDirectory.boolValue else {
+            setBodyPartError(withReason: .bodyPartFileIsDirectory(at: fileURL))
+            return
+        }
 
         //============================================================
         //          Check 4 - can the file size be extracted?
